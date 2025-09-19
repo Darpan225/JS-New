57421af7cbb07a0d0a050dd5317d2556f0863021
Darpan Biswas <141399804+Darpan225@users.noreply.github.com>
2025-09-19 03:04:59 +0000

    Learned const, let, var

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const studentID = 22548;
+let student_name = "Ramesh Das"
+var Student_Roll = "1AA25788JS"
+Student_City = "Kolkata"
+//studentID = 72511;
+// never use var, and dont declare variable without let or const
+console.table([studentID, student_name, Student_Roll, Student_City])
