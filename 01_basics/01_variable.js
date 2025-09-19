f341970e57e2fcbb6017946e6e85d121355cd721
Darpan Biswas <141399804+Darpan225@users.noreply.github.com>
2025-09-19 03:08:50 +0000

    added some comments

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -3,5 +3,6 @@ let student_name = "Ramesh Das"
 var Student_Roll = "1AA25788JS"
 Student_City = "Kolkata"
 //studentID = 72511;
-// never use var, and dont declare variable without let or const
+// never use var, because of issue in block scope and functional scope
+// and dont declare variable without let or const
 console.table([studentID, student_name, Student_Roll, Student_City])
