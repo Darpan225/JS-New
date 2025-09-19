343cdbdda603bd535e96223312030af27565d4fb
Darpan Biswas <141399804+Darpan225@users.noreply.github.com>
2025-09-19 07:24:48 +0000

    Details Datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,3 +2,11 @@
 
 /*alert (3+3) will not work here cause we are using nodejs not browser*/
  
+let Student_Name = "Akash Roy"
+let Student_Age = 22
+let Student_Subject = ["Hindi", "Ensglish", "Mathematics"]
+let Student_City;
+console.log(Student_Name, "Datatype = ", typeof(Student_Name))
+console.log(Student_Age, "Datatype = ", typeof(Student_Age))
+console.log(Student_Subject, "Datatype = ", typeof(Student_Subject))
+console.log(typeof(Student_City))
