ad247c94ffa536f00fb18ff3925fb3b5fbc0b6bf
Darpan Biswas <141399804+Darpan225@users.noreply.github.com>
2025-09-23 08:29:38 +0000

    Datatypes in details

---
+++ 01_basics/datatypes_summarry.js
@@ -0,0 +1,17 @@
+let str_name = "Ramu Das"
+let number_age = 44
+let VoteID = undefined
+let job = null
+let ctzn = true
+
+let lst = ["Mango", "Banana", "Apple", "Tomamto", "Kiwi"]
+
+console.table([str_name, number_age, VoteID, ctzn, job])
+
+console.log(typeof(str_name))
+
+console.log(typeof(number_age))
+console.log(typeof(VoteID))
+console.log(typeof(job))
+console.log(typeof(ctzn))
+console.log("\n" + typeof(lst))
