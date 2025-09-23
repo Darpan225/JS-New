6d17eea06bbaf43e7531073a49c48a57d7785a85
Darpan Biswas <141399804+Darpan225@users.noreply.github.com>
2025-09-23 15:16:59 +0000

    Basic ways of Define Strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,13 @@
+//normal string declaration 
+let ST_name= "Amit sen"
+
+//console.log(ST_name)
+//console.log(typeof(ST_name))
+
+//string declaration with object 'new' String
+ let str_name = new String ('Emran Hashmi')
+
+console.log(str_name)
+console.log(typeof(str_name))
+
+console.log(`Hii my name is ${ST_name} and i am a big fan of ${str_name}`)
