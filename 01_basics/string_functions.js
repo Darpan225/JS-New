2657eb52785eb64228705e2f1312eb81d2bdfb87
Darpan Biswas <141399804+Darpan225@users.noreply.github.com>
2025-09-23 17:17:02 +0000

    used some string functions

---
+++ 01_basics/string_functions.js
@@ -0,0 +1,13 @@
+// in this file of code we will learn string functions in details 
+
+let ST_name= "Amit sen"
+
+//console.log(ST_name)
+//console.log(typeof(ST_name))
+
+ const str_name = new String ('Emran Hashmi')
+//console.log(str_name)
+//console.log(typeof(str_name))
+//console.log(`Hii my name is ${ST_name} and i am a big fan of ${str_name}`)
+
+console.log(str_name.at(5))
