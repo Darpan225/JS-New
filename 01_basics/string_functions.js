db798e8ef5b05b5d46feb0bd2f962c2f508a0e98
Darpan Biswas <141399804+Darpan225@users.noreply.github.com>
2025-10-07 17:49:47 +0000

    new function learned

--- 01_basics/string_functions.js
+++ 01_basics/string_functions.js
@@ -11,3 +11,19 @@ console.log (str_name.charAt(2))
 
 //length of a String 
 console.log(str_name.length)
+
+//chatat()
+console.log(`The character at index 4   `)
+
+let para = "Launch your career as a front-end developer. Build job-ready skills for an in-demand career and earn a credential from Meta. No degree or prior experience required to get started."
+
+let search_termf = "as"
+let search_terml = "get"
+
+let index_first = para.indexOf(search_termf)
+let index_last = para.lastIndexOf(search_terml)
+
+console.log(`so the index of the first "${search_termf}" is ${index_first}`)
+
+console.log(`so the index of the last "${search_terml}" is ${index_last} `)
+
