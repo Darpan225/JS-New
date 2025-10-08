c0db15759e28309efe77a8e57e632414c8765499
Darpan Biswas <141399804+Darpan225@users.noreply.github.com>
2025-10-08 16:08:07 +0000

    file name change and & end of part 1 functions

--- 01_basics/string_functions_01.js
+++ 01_basics/string_functions_01.js
@@ -1,6 +1,6 @@
 // in this file of code we will learn string functions in details 
 
-let ST_name= "Amit sen"
+/*let ST_name= "Amit sen"
  const str_name = new String ('Emran Hashmi')
 //console.log(`Hii my name is ${ST_name} and i am a big fan of ${str_name}`)
 
@@ -26,4 +26,22 @@ let index_last = para.lastIndexOf(search_terml)
 console.log(`so the index of the first "${search_termf}" is ${index_first}`)
 
 console.log(`so the index of the last "${search_terml}" is ${index_last} `)
+
+//include() to check word include into the string or not
+let search_wrd = "front-end"
+console.log( para.includes(search_wrd))*/
+
+//slice() → extract part of string.
+
+let para2 = "Prime Minister Narendra Modi inaugurated the Navi Mumbai International Airport on Wednesday. "
+
+console.log(para2.slice(10))
+
+//substring() / substr() → similar to slice (know them, but slice() is preferred).
+
+let nam1 = "Narendra Modi"
+
+console.log(nam1.substring(0, 8))
+
+
 
